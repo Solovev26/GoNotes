c80fd70b80cc1bf862777e8b422c77fed014fb93
Solovev26 <solovev26.ru@gmail.com>
2023-06-30 19:35:14 +0300

    feat: Add new route

--- main.go
+++ main.go
@@ -5,14 +5,38 @@ import (
 	"net/http"
 )
 
+// Обработчик главной страницы.
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Привет мир"))
+}
+
+// Обработчик для отображения содержимого заметки.
+func showNote(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Отображение заметки..."))
+}
+
+// Обработчик для создания новой заметки.
+func createNote(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+
+		http.Error(w, "Метод запрещен!", 405)
+		return
+	}
+
+	w.Write([]byte("Форма для создания новой заметки..."))
 }
 
 func main() {
 
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/note", showNote)
+	mux.HandleFunc("/note/create", createNote)
 
 	log.Println("Запуск веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
